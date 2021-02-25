fab699c13aa89e9648827696920b64bd82623f55
Sourabh S Nath <sourabhsnath@gmail.com>
2021-02-25 11:19:11 +0530

    Switched to timber for logging

--- app/src/main/java/com/guardianangels/football/ui/players/playerlist/PlayerListFragment.kt
+++ app/src/main/java/com/guardianangels/football/ui/players/playerlist/PlayerListFragment.kt
@@ -2,22 +2,22 @@ package com.guardianangels.football.ui.players.playerlist
 
 import android.annotation.SuppressLint
 import android.graphics.drawable.InsetDrawable
-import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.guardianangels.football.R
 import com.guardianangels.football.data.Player
 import com.guardianangels.football.network.NetworkState
-import com.guardianangels.football.ui.base.ToolbarState
 import com.guardianangels.football.ui.base.BasePlayerListFragment
 import com.guardianangels.football.ui.base.SectionedPlayerListAdapter
+import com.guardianangels.football.ui.base.ToolbarState
 import com.guardianangels.football.util.Constants
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class PlayerListFragment : BasePlayerListFragment() {
@@ -39,12 +39,11 @@ class PlayerListFragment : BasePlayerListFragment() {
      * To handle recyclerview item clicks
      */
     override fun playerItemClickListener(player: Player) {
-        Log.d(TAG, "playerItemClickListener: Clicked. ${player.playerName}")
         if (viewModel.multiSelectionHandler.isMultiSelectionStateActive()) {
             viewModel.multiSelectionHandler.addOrRemovePlayersFromSelectedList(player)
-            Log.d(TAG, "playerItemClickListener: Selected")
+            Timber.tag(TAG).d("playerItemClickListener: Selected ${player.playerName}")
         } else {
-            Log.d(TAG, "playerItemClickListener: Moved to another Fragment")
+            Timber.tag(TAG).d("playerItemClickListener: Moved to another Fragment")
             findNavController().navigate(PlayerListFragmentDirections.actionPlayersToPlayerDetailsFragment(player))
         }
     }
@@ -81,14 +80,14 @@ class PlayerListFragment : BasePlayerListFragment() {
                 is NetworkState.Loading -> {
                 }
                 is NetworkState.Success -> {
-                    Log.d(TAG, "observeViewModel: Success ${it.data}")
+                    Timber.tag(TAG).d("observeViewModel: Success ${it.data}")
                     viewModel.multiSelectionHandler.clearSelectedList()
                     viewModel.multiSelectionHandler.setToolbarState(ToolbarState.NormalState)
                     resetTitle()
                     viewModel.getSectionedPlayerResultLiveData()
                 }
                 is NetworkState.Failed -> {
-                    Log.d(TAG, "observeViewModel: Failed: ${it.message}")
+                    Timber.tag(TAG).d("observeViewModel: Failed: ${it.message}")
                 }
             }
         }
