4d79c1465786f3df60be463bf0c5892f0595cf29
Sourabh S Nath <sourabhsnath@gmail.com>
2021-02-26 15:55:33 +0530

    Replaced MainRepository with Login and Team Repositories

--- app/src/main/java/com/guardianangels/football/repository/LoginRepository.kt
+++ app/src/main/java/com/guardianangels/football/repository/LoginRepository.kt
@@ -1,4 +1,52 @@
 package com.guardianangels.football.repository
 
-class LoginRepository {
+import com.google.firebase.auth.FirebaseAuth
+import com.guardianangels.football.network.NetworkState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.tasks.await
+
+class LoginRepository constructor(private val auth: FirebaseAuth) {
+
+    private companion object {
+        /* This is the only email allowed to login. The user will not have the ability to create a new account. */
+        private const val EMAIL = "admin@guardian.angels"
+    }
+
+    /**
+     * Check if the user is logged in
+     */
+    fun checkLogin() = auth.currentUser != null
+
+    /**
+     *
+     */
+    val currentUserUid = if (checkLogin()) auth.currentUser?.uid else null
+
+    /**
+     * Login user and return a flow
+     */
+    fun loginAdmin(password: String) = flow<NetworkState<Boolean>> {
+        emit(NetworkState.loading())
+        auth.signInWithEmailAndPassword(EMAIL, password).await()
+
+        // Check if the user has logged in successfully
+        if (checkLogin()) {
+            emit(NetworkState.success(true))
+        } else
+            emit(NetworkState.failed("Log in Failed. Please try again."))
+
+    }.catch {
+        emit(NetworkState.failed(it.message.toString()))
+    }.flowOn(Dispatchers.IO)
+
+
+    /**
+     * Logout
+     */
+    fun logoutAdmin() {
+        auth.signOut()
+    }
 }
--- app/src/main/java/com/guardianangels/football/repository/TeamRepository.kt
+++ app/src/main/java/com/guardianangels/football/repository/TeamRepository.kt
@@ -1,4 +1,164 @@
 package com.guardianangels.football.repository
 
-class TeamRepository {
+import android.net.Uri
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.guardianangels.football.data.Player
+import com.guardianangels.football.data.SectionedPlayerRecyclerItem
+import com.guardianangels.football.network.NetworkState
+import com.guardianangels.football.util.Converters.Companion.getPlayerTypeString
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ * Repository to handle getting Team Details from firestore and storage.
+ * Extends BaseRepository.
+ */
+class TeamRepository @Inject constructor(
+    firestore: FirebaseFirestore,
+    private val storage: FirebaseStorage,
+    val loginRepository: LoginRepository
+) {
+
+    private companion object {
+        private const val TAG = "TeamRepository"
+    }
+
+    /**
+     * Reference to the players firestore collection
+     */
+    private val playerCollectionRef = firestore.collection("players")
+    private val storageReference = storage.reference
+
+    /**
+     * Add players to firebase cloud firestore.
+     * Emitting a reference to the document along with network states.
+     */
+    fun addPlayer(player: Player, uri: Uri) = flow {
+        emit(NetworkState.loading())
+
+        player.setImage(uri)
+        playerCollectionRef.add(player).await()
+
+        emit(NetworkState.success(true))
+
+    }.catch {
+        emit(NetworkState.failed(it.message.toString()))
+    }.flowOn(Dispatchers.IO)
+
+
+    /**
+     * Update the player.
+     */
+    fun updatePlayer(player: Player, uri: Uri?) = flow {
+        emit(NetworkState.loading())
+
+        if (uri != null) {
+            storage.getReferenceFromUrl(player.remoteUri!!).delete().await() // First delete the previous image from storage.
+            player.setImage(uri)
+        }
+
+        var playerModel: Player? = null
+        playerCollectionRef.document(player.id!!).set(player).onSuccessTask {
+            playerCollectionRef.document(player.id!!).get()
+                .addOnSuccessListener {
+                    playerModel = it.toObject(Player::class.java)?.setID(it.id)!!
+                }
+        }.await()
+
+        emit(NetworkState.success(playerModel))
+    }.catch {
+        emit(NetworkState.failed(it.message.toString()))
+    }.flowOn(Dispatchers.IO)
+
+
+    /**
+     * Set the image url after uploading it to storage.
+     */
+    private suspend fun Player.setImage(uri: Uri) {
+        val playerImageReference = storageReference.child("PlayerImages/${loginRepository.currentUserUid}/${uri.lastPathSegment}")
+
+        val playerImageDownloadUrl = playerImageReference.putFile(uri)
+            .await()
+            .storage
+            .downloadUrl
+            .await()
+            .toString()
+
+        remoteUri = playerImageDownloadUrl
+    }
+
+
+    /**
+     * Get a list of all Players
+     */
+    fun getPlayers() = flow<NetworkState<List<SectionedPlayerRecyclerItem>>> {
+
+        emit(NetworkState.loading())
+
+        /*
+         * Returns a map -> Map<Type, List<Player>>
+         */
+        val playersSectionGroup = playerCollectionRef.get().await().map {
+            it.toObject(Player::class.java).setID(it.id)
+        }.groupBy { it.playerType }.toSortedMap(compareBy { playerType ->
+            playerType
+        })
+
+        /*
+         * Take the Map and turn it into List<SectionedPlayerRecyclerItem>
+         * Coach [Header]
+         *  Player
+         * Forward [Header]
+         *  Player
+         *  Player
+         */
+        val sectionedPlayerRecyclerItem = ArrayList<SectionedPlayerRecyclerItem>()
+        for (type in playersSectionGroup.keys) {
+
+            val playerType = type!!.getPlayerTypeString()
+
+            playerType.let { sectionedPlayerRecyclerItem.add(SectionedPlayerRecyclerItem.PlayerTypeItem(playerType)) }
+            for (player in playersSectionGroup.getValue(type)) {
+                sectionedPlayerRecyclerItem.add(SectionedPlayerRecyclerItem.PlayerItem(player))
+            }
+        }
+
+        emit(NetworkState.success(sectionedPlayerRecyclerItem))
+    }.catch {
+        emit(NetworkState.failed(it.message.toString()))
+    }.flowOn(Dispatchers.IO)
+
+
+    /**
+     * Delete multiple selected players
+     */
+    fun deleteMultiplePlayers(players: ArrayList<Player>) = flow {
+
+        emit(NetworkState.loading())
+        for (player in players) {
+            playerCollectionRef.document(player.id!!).delete().onSuccessTask {
+                // When deleting the player is successful
+                Timber.tag(TAG).d("deleteMultiplePlayers: ${player.playerName} Deleted, continue with storage deletion")
+                storage.getReferenceFromUrl(player.remoteUri!!).delete() // Delete using the url, since no direct path is available
+            }.await()
+        }
+        emit(NetworkState.success(true))
+    }.catch {
+        emit(NetworkState.failed(it.message.toString()))
+    }.flowOn(Dispatchers.IO)
+
+
+    /**
+     * Set player id, get the id from the query doc and set it to the object
+     */
+    private fun Player.setID(id: String): Player {
+        this.id = id
+        return this
+    }
 }
