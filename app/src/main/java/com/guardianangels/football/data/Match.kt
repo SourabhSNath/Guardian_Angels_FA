c820f2b5b6daf55634cb02fdb0732269b3c3213f
Sourabh S Nath <sourabhsnath@gmail.com>
2021-02-27 10:41:31 +0530

    Change Match model

--- app/src/main/java/com/guardianangels/football/data/Match.kt
+++ app/src/main/java/com/guardianangels/football/data/Match.kt
@@ -2,13 +2,13 @@ package com.guardianangels.football.data
 
 import com.google.firebase.firestore.Exclude
 
-data class UpcomingMatch(
+data class Match(
     val team1Name: String? = "",
     val team2Name: String? = "",
     var team1Logo: String? = "",
     var team2Logo: String? = "",
     val dateAndTime: Long? = null,
-    val team1Team: List<Player>? = null,
+    val team1TeamIds: List<String>? = null,
 
     @get:Exclude // Exclude the id since it's only set at the time of download
     val matchID: String? = "",
