a20295e15840a70892de4fe39a4e6064ebf5d63f
Sourabh S Nath <sourabhsnath@gmail.com>
2021-03-03 19:46:44 +0530

    Updated Model

--- app/src/main/java/com/guardianangels/football/data/Match.kt
+++ app/src/main/java/com/guardianangels/football/data/Match.kt
@@ -1,15 +1,21 @@
 package com.guardianangels.football.data
 
+import android.os.Parcelable
 import com.google.firebase.firestore.Exclude
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Match(
     val team1Name: String? = "",
     val team2Name: String? = "",
     var team1Logo: String? = "",
     var team2Logo: String? = "",
     val dateAndTime: Long? = null,
+    var tournamentName: String? = "",
+    var locationName: String? = "",
     val team1TeamIds: List<String>? = null,
+    var isCompleted: Boolean? = null,
 
     @get:Exclude // Exclude the id since it's only set at the time of download
-    val matchID: String? = "",
-)
+    var matchID: String? = "",
+) : Parcelable
