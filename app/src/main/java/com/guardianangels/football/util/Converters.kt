fe440a465c6d7c7e7372e3a8b80f5f3ef8b0249f
Sourabh S Nath <sourabhsnath@gmail.com>
2021-03-13 20:32:13 +0530

    Removed class

--- app/src/main/java/com/guardianangels/football/util/Converters.kt
+++ app/src/main/java/com/guardianangels/football/util/Converters.kt
@@ -2,27 +2,23 @@ package com.guardianangels.football.util
 
 import com.guardianangels.football.data.PlayerType
 
-class Converters {
-    companion object {
 
-        fun PlayerType.getPlayerTypeString(singular: Boolean = false): String {
-            return when (this) {
-                PlayerType.GOAL_KEEPER -> if (!singular) "${Constants.GOAL_KEEPER}s" else Constants.GOAL_KEEPER
-                PlayerType.DEFENDER -> if (!singular) "${Constants.DEFENDER}s" else Constants.DEFENDER
-                PlayerType.MIDFIELDER -> if (!singular) "${Constants.MIDFIELDER}s" else Constants.MIDFIELDER
-                PlayerType.FORWARD -> if (!singular) "${Constants.FORWARD}s" else Constants.FORWARD
-                PlayerType.COACH -> if (!singular) Constants.COACH else Constants.COACH
-            }
-        }
+fun PlayerType.getPlayerTypeString(singular: Boolean = false): String {
+    return when (this) {
+        PlayerType.GOAL_KEEPER -> if (!singular) "${Constants.GOAL_KEEPER}s" else Constants.GOAL_KEEPER
+        PlayerType.DEFENDER -> if (!singular) "${Constants.DEFENDER}s" else Constants.DEFENDER
+        PlayerType.MIDFIELDER -> if (!singular) "${Constants.MIDFIELDER}s" else Constants.MIDFIELDER
+        PlayerType.FORWARD -> if (!singular) "${Constants.FORWARD}s" else Constants.FORWARD
+        PlayerType.COACH -> if (!singular) Constants.COACH else Constants.COACH
+    }
+}
 
-        fun String.getPlayerType(): PlayerType {
-            return when (this) {
-                "Goal Keeper" -> PlayerType.GOAL_KEEPER
-                "Defender" -> PlayerType.DEFENDER
-                "Midfielder" -> PlayerType.MIDFIELDER
-                "Forward" -> PlayerType.FORWARD
-                else -> PlayerType.COACH
-            }
-        }
+fun String.getPlayerType(): PlayerType {
+    return when (this) {
+        "Goal Keeper" -> PlayerType.GOAL_KEEPER
+        "Defender" -> PlayerType.DEFENDER
+        "Midfielder" -> PlayerType.MIDFIELDER
+        "Forward" -> PlayerType.FORWARD
+        else -> PlayerType.COACH
     }
 }
