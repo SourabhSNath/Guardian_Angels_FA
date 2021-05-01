233180024a6d6e617839065cecd1dde81c78a2e7
Sourabh S Nath <sourabhsnath@gmail.com>
2021-05-01 20:53:59 +0530

    Gallery image model

--- app/src/main/java/com/guardianangels/football/data/Picture.kt
+++ app/src/main/java/com/guardianangels/football/data/Picture.kt
@@ -1,3 +1,17 @@
 package com.guardianangels.football.data
 
-data class Picture()
+import android.os.Parcelable
+import androidx.annotation.Keep
+import com.google.firebase.firestore.Exclude
+import kotlinx.parcelize.Parcelize
+
+@Keep
+@Parcelize
+data class Picture(
+    var imageLink: String? = "",
+    var imageDescription: String = "",
+    var dateAndTime: Long? = null,
+
+    @get:Exclude // Exclude the id since it's only set at the time of download
+    var id: String? = null
+) : Parcelable
